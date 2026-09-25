467fa33828d0f03a0bc1ea67e81c6ff8dcdbbb61
dhairm92-oss <dhairm92@gmail.com>
2026-09-25 10:08:32 +0300

    Add lib/models/coffee_item.dart

---
+++ lib/models/coffee_item.dart
@@ -0,0 +1,17 @@
+class CoffeeItem {
+  final String id;
+  final String name;
+  final String category;
+  final double price;
+  final String description;
+  final String imageUrl;
+
+  CoffeeItem({
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.description,
+    required this.imageUrl,
+  });
+}
